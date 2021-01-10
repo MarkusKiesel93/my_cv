3d9924f2ff1d3f7c86112a57b4b6d7836391d291
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-01-10 13:41:48 +0100

    remove custom colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
-import light from './theme'
-
 Vue.use(Vuetify);
 
-export default new Vuetify({
-    theme: {
-        themes: { light },
-      },
-});
+export default new Vuetify({});
