3163a465c6679f56f7f834f6fea678df9ad9c14a
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-02-14 11:34:58 +0100

    serve images with nginx for development

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
+
+let BASE_PATH = process.env.BASE_URL + 'static/'
+if (process.env.NODE_ENV === 'development') {
+  BASE_PATH = 'http://localhost:8001/static/'
+}
 
 export default new Vuex.Store({
   state: {
     base: {
       name: 'Markus Kiesel BSc.',
       occupation: 'Zukünftiger Data Scientist',
-      profileImage: require('@/assets/images/profile.jpg'),
-      backgroundImage: require('@/assets/images/background.jpg'),
+      profileImage: BASE_PATH + 'images/profile.jpg',
+      backgroundImage: BASE_PATH + 'images/background.jpg',
       links: [
         {
           id: 1,
-          text: "Git Hub",
-          link: "https://github.com/MarkusKiesel93",
-          icon: "mdi-github",
+          text: 'Git Hub',
+          link: 'https://github.com/MarkusKiesel93',
+          icon: 'mdi-github',
         },
         {
           id: 2,
-          text: "Download",
-          link: "http://cv.pdf.kiesel.tech",
-          icon: "mdi-pdf-box",
+          text: 'Download',
+          link: 'http://cv.pdf.kiesel.tech',
+          icon: 'mdi-pdf-box',
         },
       ]
     },
@@ -31,7 +36,7 @@ export default new Vuex.Store({
         title: 'Masterstudium Data Science',
         from: '2019',
         institution: 'Technische Universität Wien',
-        image: require('@/assets/images/study/tu.png'),
+        image: BASE_PATH + 'images/study/tu.png',
         description: [
           'derzeit laufendes Studium'
         ],
@@ -42,7 +47,7 @@ export default new Vuex.Store({
         from: '2013',
         to: '2018',
         institution: 'Wirtschaftsuniversität Wien',
-        image: require('@/assets/images/study/wu.png'),
+        image: BASE_PATH + 'images/study/wu.png',
         description: [
           'Abschluss: Bachelor of Science (WU)',
           'Spezialisierung 1: Information Management and Control',
@@ -59,7 +64,7 @@ export default new Vuex.Store({
         description: [
           'als Zweitstudium geführt aber nicht abgeschlossen',
         ],
-        image: require('@/assets/images/study/tu.png'),
+        image: BASE_PATH + 'images/study/tu.png',
       },
       {
         id: 4,
@@ -71,7 +76,7 @@ export default new Vuex.Store({
           'Abschluss: Reife- und Diplomprüfung mit gutem Erfolg',
           'Spezialisierung: Informationstechnik und Digital Management',
         ],
-        image: require('@/assets/images/study/hak.png'),
+        image: BASE_PATH + 'images/study/hak.png',
       },
     ],
     work: [
@@ -81,7 +86,7 @@ export default new Vuex.Store({
         from: '2019',
         to: '2021',
         institution: 'Donau Versicherungs AG',
-        image: require('@/assets/images/work/donau.png'),
+        image: BASE_PATH + 'images/work/donau.png',
         description: [
           'Implementierung eines Rechenkernes für die Tarifmathematik (Java)',
           'Unterstützung bei der Entwicklung einer Testumgebung (Java, VBA)',
@@ -94,7 +99,7 @@ export default new Vuex.Store({
         from: '2013',
         to: '2019',
         institution: 'Donau Versicherungs AG',
-        image: require('@/assets/images/work/donau.png'),
+        image: BASE_PATH + 'images/work/donau.png',
         description: [
           'Automatisierung von Berechnungen und Datenerfassung (VBA, PowerShell)',
           'Antragserfassung: Erfassen von vertragsspezifischen Daten in SAP',
@@ -107,13 +112,13 @@ export default new Vuex.Store({
         id: 1,
         title: 'Deutsch',
         level: 'Muttersprache',
-        image: require('@/assets/images/language/german.png'),
+        image: BASE_PATH + 'images/language/german.png',
       },
       {
         id: 2,
         title: 'Englisch',
         level: 'Sehr gut in Wort und Schrift (C1)',
-        image: require('@/assets/images/language/english.png'),
+        image: BASE_PATH + 'images/language/english.png',
       },
     ],
     skills: [
@@ -121,49 +126,49 @@ export default new Vuex.Store({
         id: 1,
         title: 'Python',
         level: 90,
-        image: require('@/assets/images/skill/python.png'),
+        image: BASE_PATH + 'images/skill/python.png',
       },
       {
         id: 2,
         title: 'Java',
         level: 70,
-        image: require('@/assets/images/skill/java.png'),
+        image: BASE_PATH + 'images/skill/java.png',
       },
       {
         id: 3,
         title: 'JavaScript',
         level: 40,
-        image: require('@/assets/images/skill/js.png'),
+        image: BASE_PATH + 'images/skill/js.png',
       },
       {
         id: 4,
         title: 'R',
         level: 60,
-        image: require('@/assets/images/skill/r.png'),
+        image: BASE_PATH + 'images/skill/r.png',
       },
       {
         id: 5,
         title: 'VBA',
         level: 70,
-        image: require('@/assets/images/skill/vba.png'),
+        image: BASE_PATH + 'images/skill/vba.png',
       },
       {
         id: 6,
         title: 'SQL',
         level: 75,
-        image: require('@/assets/images/skill/sql.png'),
+        image: BASE_PATH + 'images/skill/sql.png',
       },
       {
         id: 7,
         title: 'Bash script',
         level: 30,
-        image: require('@/assets/images/skill/bash.png'),
+        image: BASE_PATH + 'images/skill/bash.png',
       },
       {
         id: 8,
         title: 'PowerShell',
         level: 30,
-        image: require('@/assets/images/skill/powershell.png'),
+        image: BASE_PATH + 'images/skill/powershell.png',
       },
     ],
     projects: [
@@ -171,19 +176,19 @@ export default new Vuex.Store({
         id: 1,
         title: 'Semantic System',
         text: 'Programmieren einer Messungsumgebung zur Auswertung verschiedener Triplestores (RDF Datenbanken) in Java und Auswerten der Messdaten im Zuge meiner Bachelorarbeit.',
-        image: require('@/assets/images/project/semantic_web.png'),
+        image: BASE_PATH + 'images/project/semantic_web.png',
       },
       {
         id: 2,
         title: 'Machine Learning',
         text: 'Analyse von Datensätzen, Anwenden von Klassifikations- und Regressionmodellen an verschiedenen Datensätzen und Klassifizierung von Bildern in Python (NumPy, Pandas, scikit-learn, Keras, TensorFlow) im Zuge eines Machine Learning Projektes.',
-        image: require('@/assets/images/project/machine_learning.png'),
+        image: BASE_PATH + 'images/project/machine_learning.png',
       },
       {
         id: 3,
         title: 'Webentwicklung',
         text: 'Entwerfen und Entwickeln einer Webpage. Im Frontend JavaScript mit dem Framework Vue.js, im Backend Python mit dem Framework Django und als Datenbank PostgreSQL.',
-        image: require('@/assets/images/project/web_dev.png'),
+        image: BASE_PATH + 'images/project/web_dev.png',
       },
     ],
     knowledge: [
@@ -231,7 +236,7 @@ export default new Vuex.Store({
         id: 1,
         title: 'Process Analyst',
         description: 'Prüfung zum "Process Anlayst" am 31.01.2017 ausgestellt von der Gesellschaft für Prozessmanagement in Kooperation mit der WU Wien.',
-        image:  require('@/assets/images/certificates/processmanagement.png'),
+        image:  BASE_PATH + 'images/certificates/processmanagement.png',
       },
     ]
   },
